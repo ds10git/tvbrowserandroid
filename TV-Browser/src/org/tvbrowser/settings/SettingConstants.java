e9b535b1c094c6cb916d76951b2df048bdf4a3d1
ds10 <rene@tvbrowser.org>
2013-10-01 17:40:43 +0200

    Allow selection of download range.

--- TV-Browser/src/org/tvbrowser/settings/SettingConstants.java
+++ TV-Browser/src/org/tvbrowser/settings/SettingConstants.java
@@ -4,4 +4,5 @@ public class SettingConstants {
   public static final String SUBSCRIBED_CHANNELS = "SUBSCRIBED_CHANNELS";
   public static final String EPG_FREE_KEY = "EPG_FREE";
   public static final String[] LEVEL_NAMES = {"base","more00-16","more16-00","picture00-16","picture16-00"}; 
+  public static final int[] DOWNLOAD_DAYS = {0,1,2,3,7,14};
 }
