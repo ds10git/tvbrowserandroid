410c37d2f803dd3fb746793b8b8cf6dc794152b2
ds10 <rene@tvbrowser.org>
2013-10-21 21:48:33 +0200

    Show origin, year, actors and website link in details.

--- TV-Browser/src/org/tvbrowser/tvbrowser/ProgramTableFragment.java
+++ TV-Browser/src/org/tvbrowser/tvbrowser/ProgramTableFragment.java
@@ -636,7 +636,7 @@ public class ProgramTableFragment extends Fragment {
     }
     
     select.setMinDate(dayStart - 24 * 60 * 60 * 1000);
-    select.setMaxDate(dayStart + 14 * (24 * 60 * 60 * 1000));
+    select.setMaxDate(dayStart + 15 * (24 * 60 * 60 * 1000));
     select.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), null);
     
     builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
