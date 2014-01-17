beec89b257a55249f71a0226f5107ad17d2dded5
ds10 <rene@tvbrowser.org>
2014-01-17 10:58:33 +0100

    Fix NPE after data update. Disable synchro menu items until account data is entered.

--- TV-Browser/src/org/tvbrowser/tvbrowser/UiUtils.java
+++ TV-Browser/src/org/tvbrowser/tvbrowser/UiUtils.java
@@ -52,6 +52,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.Bitmap.Config;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -1485,5 +1486,18 @@ public class UiUtils {
         }
       });
     }
+  }
+  
+  public static Bitmap drawableToBitmap (Drawable drawable) {
+    if (drawable instanceof BitmapDrawable) {
+        return ((BitmapDrawable)drawable).getBitmap();
+    }
+
+    Bitmap bitmap = Bitmap.createBitmap(drawable.getBounds().width(), drawable.getBounds().height(), Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap); 
+    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+    drawable.draw(canvas);
+
+    return bitmap;
   }
 }
